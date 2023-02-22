9ba37ed37045742ee98998e13f4c1f89b07cb9a0
imartinr <immartin@profile.es>
2023-02-22 01:08:28 +0100

    Add tailwindcss in client project

--- client/src/app/bootstrap/bootstrap.router.ts
+++ client/src/app/bootstrap/bootstrap.router.ts
@@ -4,5 +4,9 @@ export const BootstrapRouter = RouterModule.forRoot([
     {
         path: '',
         loadChildren: () => import('../pages/catalog').then(m => m.CatalogModule),
+    },
+    {
+        path: ':id',
+        loadChildren: () => import('../pages/detail').then(m => m.DetailModule),
     }
 ]);
